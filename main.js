8ee7d8bed7bab2e6d32d8ff62ca57d074b378443
Iord Neto <iordneto95@gmail.com>
2022-05-22 22:05:56 -0300

    feat: add the main functionalities - add, remove and done to-do item

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+/* Variables */
+let todoInput = document.querySelector(".todo-input");
+let addTodoButton = document.querySelector(".add-todo-btn");
+let todoList = document.querySelector(".todo-list");
+let doneButtonList = document.querySelectorAll(".todo-done-btn");
+let trashButtonList = document.querySelectorAll(".todo-trash-btn");
+
+/* Listeners */
+addTodoButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addTodo(todoInput.value);
+});
+
+/* Functions */
+const addTodo = (text) => {
+  const newTodoDiv = document.createElement("div");
+
+  const todoItem = document.createElement("li");
+  todoItem.innerText = text;
+  newTodoDiv.appendChild(todoItem);
+
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "done";
+  doneButton.classList.add("todo-done-btn");
+  doneButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.target.parentElement.classList.add("done");
+  });
+  newTodoDiv.appendChild(doneButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerText = "trash";
+  trashButton.classList.add("todo-trash-btn");
+  trashButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.target.parentElement.remove();
+  });
+  newTodoDiv.appendChild(trashButton);
+
+  todoList.appendChild(newTodoDiv);
+};
